1a07f7835c0fb2b3a4fb793f00fde9de84525ccb
wamsee <wamsee@users.noreply.github.com>
2015-05-24 00:10:04 +0100

    Update LexicalScoping.R

--- LexicalScoping.R
+++ LexicalScoping.R
@@ -11,8 +11,8 @@
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
-    x <<- y
-    inv <<- NULL
+        x <<- y
+        inv <<- NULL
   }
   get <- function() x
   setinverse <- function(inverse) inv <<- inverse
@@ -22,4 +22,21 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse=getinverse)
 }
 
+# The following function returns the inverse of the matrix. 
+# It first checks if the inverse has already been computed. If yes, the result is obtained and the computation is
+# skipped, else , it computes the inverse, sets the value in the cache using setinverse function.
+# This function assumes that the matrix is always invertible.
 
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
+
+#End of Code
